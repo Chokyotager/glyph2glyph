fb2a2deff727a62c2184aecb99bbbbb4d0d5789d
ChocoParrot <lachocoparrot@gmail.com>
2020-08-27 14:25:19 +0800

    [re/factor] changes

--- model.py
+++ model.py
@@ -7,15 +7,19 @@ class Model ():
 
         input = keras.Input(shape=(128, 128, 1))
 
-        conv_1 = keras.layers.Conv2D(64, 7, strides=3, activation=keras.activations.selu)(input)
-        conv_2 = keras.layers.Conv2D(64, 5, strides=3, activation=keras.activations.selu)(conv_1)
-        conv_3 = keras.layers.Conv2D(64, 3, strides=3, activation=keras.activations.selu)(conv_2)
+        conv_1 = keras.layers.Conv2D(64, 7, strides=3, activation=keras.activations.selu, padding="valid")(input)
+        conv_2 = keras.layers.Conv2D(64, 5, strides=3, activation=keras.activations.selu, padding="valid")(conv_1)
+        conv_3 = keras.layers.Conv2D(64, 3, strides=3, activation=keras.activations.selu, padding="valid")(conv_2)
 
-        deconv_1 = keras.layers.Conv2DTranspose(64, 3, strides=3, activation=keras.activations.selu)(conv_3)
-        deconv_2 = keras.layers.Conv2DTranspose(64, 3, strides=3, activation=keras.activations.selu)(deconv_1)
-        output = keras.layers.Conv2DTranspose(64, 3, strides=3, activation=keras.activations.selu)(deconv_2)
+        deconv_1 = keras.layers.Conv2DTranspose(64, 3, strides=3, activation=keras.activations.selu, padding="valid")(conv_3)
+        deconv_2 = keras.layers.Conv2DTranspose(64, 5, strides=3, activation=keras.activations.selu, padding="valid")(deconv_1)
+        deconv_3 = keras.layers.Conv2DTranspose(64, 7, strides=3, activation=keras.activations.selu, padding="valid")(deconv_2)
+        output = keras.layers.Conv2DTranspose(1, 11, strides=1, activation=keras.activations.tanh, padding="valid")(deconv_3)
 
         model = keras.Model(inputs=input, outputs=output)
-        model.compile(optimizer=keras.optimizers.Adam, loss=tf.losses.mean_pairwise_squared_error, metrics=["mse"])
+
+        optimiser = keras.optimizers.Adam(learning_rate=0.001, beta_1=0.91, beta_2=0.999)
+
+        model.compile(optimizer=optimiser, loss=keras.losses.MSLE, metrics=["MeanSquaredLogarithmicError"])
 
         self.model = model
